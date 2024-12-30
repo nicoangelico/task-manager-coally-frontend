13cd1e31ffaa1e56da382b4fa8d2c0040191359c
nicoangelico <nangelico@membranelabs.com>
2024-12-30 13:57:51 -0300

    fix env var on nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    API_URL: process.env.API_URL || 'http://localhost:8000/api',
+  },
 };
 
 export default nextConfig;
