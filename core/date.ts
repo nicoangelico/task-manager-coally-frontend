fd9dbb5a1ace66c7f73de2f3f2d42b614447c03d
nicoangelico <nangelico@membranelabs.com>
2024-12-30 13:33:31 -0300

    fix dateFormatMMDDYYYY function

--- core/date.ts
+++ core/date.ts
@@ -1,5 +1,5 @@
 export const dateFormatMMDDYYYY = (date: string) => {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
   const dateFormat = new Date(date);
   return dateFormat.toLocaleDateString('en-US', options);
 };
